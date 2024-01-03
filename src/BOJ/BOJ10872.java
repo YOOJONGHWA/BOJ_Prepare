ad744c6b23826f4bfccdb7a3d521b8aaaa544163
YOOJONGHWA <sola120731@gmail.com>
2024-01-04 00:04:29 +0900

    깃연동 테스트

--- src/BOJ/BOJ10872.java
+++ src/BOJ/BOJ10872.java
@@ -8,27 +8,71 @@ import java.util.StringTokenizer;
 public class BOJ10872 {
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//
+//        int N = Integer.parseInt(br.readLine());
+//        int[] arr = new int[N];
+//
+//        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+//        int minValue = Integer.MAX_VALUE; // 초기값을 최대값으로 설정
+//        int maxValue = Integer.MIN_VALUE; // 초기값을 최소값으로 설정
+//
+//        for (int i = 0; i < N; i++) {
+//            arr[i] = Integer.parseInt(st.nextToken());
+//
+//            if (maxValue < arr[i]) {
+//                maxValue = arr[i];
+//            }
+//
+//            if (minValue > arr[i]) {
+//                minValue = arr[i];
+//            }
+//        }
+//
+//        System.out.println(minValue + " " + maxValue);
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
         int[] arr = new int[N];
-
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int minValue = Integer.MAX_VALUE; // 초기값을 최대값으로 설정
-        int maxValue = Integer.MIN_VALUE; // 초기값을 최소값으로 설정
-
-        for (int i = 0; i < N; i++) {
+        st = new StringTokenizer(br.readLine(), " ");
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for(int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
+            System.out.println(arr[i]);
 
-            if (maxValue < arr[i]) {
-                maxValue = arr[i];
+            if(max < arr[i]) {
+                max = arr[i];
+            }
+            if(min > arr[i]) {
+                min = arr[i];
             }
 
-            if (minValue > arr[i]) {
-                minValue = arr[i];
-            }
         }
+        System.out.println(max + " " + min);
 
-        System.out.println(minValue + " " + maxValue);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
