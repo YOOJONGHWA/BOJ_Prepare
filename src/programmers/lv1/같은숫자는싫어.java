2985e44f5183d58de1f01518dd4549d8596b32a2
YOOJONGHWA <sola120731@gmail.com>
2024-02-27 21:35:24 +0900

    다시 차근 차근

--- src/programmers/lv1/같은숫자는싫어.java
+++ src/programmers/lv1/같은숫자는싫어.java
@@ -1,38 +1,31 @@
 package programmers.lv1;
 
-
-import java.util.Stack;
+import java.util.*;
 
 public class 같은숫자는싫어 {
-    public int[] solution(int []arr) {
+    public int[] solution(int[] arr) {
 
         Stack<Integer> stack = new Stack<>();
-
-        for (int num : arr) {
-
-            if (!stack.isEmpty() || stack.peek() == num) {
+        for (int i = 0; i < arr.length; i++) {
+            if (!stack.isEmpty() && stack.peek() == arr[i]) {
                 stack.pop();
             }
-            else {
-                stack.push(num);
-            }
-
+            stack.push(arr[i]);
         }
         int[] answer = new int[stack.size()];
-        for (int i = 0; i < stack.size(); i++) {
+        for(int i = answer.length - 1; i >= 0; i--) {
             answer[i] = stack.pop();
         }
-
         return answer;
     }
     public static void main(String[] args) {
-        int[] arr = {1,1,3,3,0,1,1};
+        int[] arr = {1,3,0,1};
         int[] arr2 = {4,4,4,3,3};
 
         같은숫자는싫어 sol = new 같은숫자는싫어();
 
-        System.out.println(sol.solution(arr));
-        System.out.println(sol.solution(arr2));
+        System.out.println(Arrays.toString(sol.solution(arr)));
+        System.out.println(Arrays.toString(sol.solution(arr2)));
 
     }
 }
