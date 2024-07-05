1682a83153571c1c086eabbce61a5aca9c82ef48
유종화 <sola201@naver.com>
2024-07-05 13:13:35 +0900

    다시 차근 차근

---
+++ src/codeUp/code1011.java
@@ -0,0 +1,13 @@
+package codeUp;
+
+import java.util.Scanner;
+
+public class code1011 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char ch = sc.next().charAt(0);
+        System.out.println(ch);
+    }
+
+}
