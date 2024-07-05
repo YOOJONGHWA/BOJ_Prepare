e604e2955ccd21c8568bc90cfd3aa2ebbcf49256
유종화 <sola201@naver.com>
2024-07-05 13:34:41 +0900

    다시 차근 차근

---
+++ src/codeUp/code1150.java
@@ -0,0 +1,21 @@
+package codeUp;
+
+import java.util.Scanner;
+
+public class code1150 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int min = a;
+        if(b < min) {
+            min = b;
+        }
+        if(c < min) {
+            min = c;
+        }
+        System.out.println(min);
+    }
+}
