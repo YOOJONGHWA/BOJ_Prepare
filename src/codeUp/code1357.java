895a5674de124c24b9302b0196757bd03d1e3722
유종화 <sola201@naver.com>
2024-07-07 17:51:18 +0900

    도형문자 집중 탐구

---
+++ src/codeUp/code1357.java
@@ -0,0 +1,28 @@
+package codeUp;
+
+import java.util.Scanner;
+
+public class code1357 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int length = a + (a - 1);
+
+        for (int i = 0; i < length; i++) {
+            if (i < a) {
+                for (int j = 0; j <= i; j++) {
+                    System.out.print("*");
+                }
+            }
+            else {
+                for (int j = 0; j < length - i; j++) {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+}
