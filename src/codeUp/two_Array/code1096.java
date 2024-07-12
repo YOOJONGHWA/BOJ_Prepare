eb378ef7ee7fd7b4479a507305cef654344b3fe9
유종화 <sola201@naver.com>
2024-07-13 01:03:03 +0900

    다시 차근 차근

--- src/codeUp/two_Array/code1096.java
+++ src/codeUp/two_Array/code1096.java
@@ -1,11 +1,33 @@
 package codeUp.two_Array;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
 public class code1096 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] arr = new int[19][19];
 
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken()) - 1;
+            int y = Integer.parseInt(st.nextToken()) - 1;
+            arr[x][y] = 1;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 
 }
+ 
