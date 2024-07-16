4ad98a3da9bb06a7136ed820ea265b14e1e0b494
유종화 <sola201@naver.com>
2024-07-17 00:15:30 +0900

    다시 차근 차근

--- src/codeUp/BJ10807.java
+++ src/codeUp/BJ10807.java
@@ -13,13 +13,11 @@ public class BJ10807 {
         int n = Integer.parseInt(br.readLine());
         int[] arr = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
         int v = Integer.parseInt(br.readLine());
         int count = 0;
-        for(int num : arr) {
-            if(num == v) {
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            if(arr[i] == v) {
                 count++;
             }
         }
