2ccfe6cf550cdc9ae876c5fc7545f3bb4d9e8d20
유종화 <121346218+YOOJONGHWA@users.noreply.github.com>
2024-08-01 22:43:31 +0900

    [Bronze III] Title: 최소, 최대, Time: 504 ms, Memory: 88396 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,5 +1,4 @@
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,23 +8,20 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        
         int n = Integer.parseInt(br.readLine());
-
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
-
+        
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
+        
+        int max = Integer.parseInt(st.nextToken());
+        int min = max;
+        
+        for (int i = 1; i < n; i++) {
             int num = Integer.parseInt(st.nextToken());
-            if (num > max) {
-                max = num;
-            }
-            if (num < min) {
-                min = num;
-            }
+            max = Math.max(max, num);
+            min = Math.min(min, num);
         }
-
+        
         System.out.println(min + " " + max);
     }
 }
