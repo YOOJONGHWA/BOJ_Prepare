93b0bd65d93ae8fe93684d18a6a464bce8e05a70
유종화 <121346218+YOOJONGHWA@users.noreply.github.com>
2024-11-17 17:45:30 +0900

    [Silver I] Title: 최대공약수, Time: 268 ms, Memory: 39176 KB -BaekjoonHub

---
+++ 백준/Silver/1850. 최대공약수/최대공약수.java
@@ -0,0 +1,38 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+        Long N = Long.parseLong(st.nextToken());
+        Long M = Long.parseLong(st.nextToken());
+
+        Long gcdNum = gcd(N,M);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < gcdNum; i++) {
+            sb.append("1");
+        }
+
+        System.out.println(sb.toString());
+
+    }
+    public static Long gcd(Long a, Long b) {
+
+        if (b == 0) {
+            return a;
+        }
+        else {
+            return gcd(b, a % b);
+        }
+    }
+
+}
