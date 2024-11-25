9ee8f26389af9d963f8667d6686f279f9c9a9298
유종화 <sola201@naver.com>
2024-11-25 18:23:55 +0900

    다시 차근 차근

---
+++ src/BOJ/test/BOJ1135.java
@@ -0,0 +1,22 @@
+package BOJ.test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ1135 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        while (true) {
+            String letter = br.readLine();
+            if (letter.equals("END")) break;
+            sb.append(new StringBuilder(letter).reverse().append("\n"));
+        }
+        System.out.print(sb);
+    }
+
+}
