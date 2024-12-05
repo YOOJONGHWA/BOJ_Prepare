e2be70324d087f3c3eab6f14e3824accf4d4c677
유종화 <sola201@naver.com>
2024-12-05 17:55:57 +0900

    다시 차근 차근

---
+++ src/BOJ/test/BOJ14425.java
@@ -0,0 +1,33 @@
+package BOJ.test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class BOJ14425 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        HashSet<String> set = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            set.add(br.readLine());
+        }
+
+        int count = 0;
+        for (int i = 0; i < m; i++) {
+            if (set.contains(br.readLine())) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+
+}
