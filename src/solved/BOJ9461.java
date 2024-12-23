615558761abe393c66ed860bdc079d3fde9d85da
유종화 <sola201@naver.com>
2024-12-23 17:03:34 +0900

    다시 차근 차근

--- src/solved/BOJ9461.java
+++ src/solved/BOJ9461.java
@@ -6,28 +6,35 @@ import java.io.InputStreamReader;
 
 public class BOJ9461 {
 
-    static Integer[] dp = new Integer[101];
+    static Long[] dp = new Long[101];
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
 
-        dp[1] = 1;
-        dp[2] = 1;
-        dp[3] = 1;
-        dp[4] = 2;
-        dp[5] = 2;
-
-        for (int i = 6; i <= 100; i++) {
-            dp[i] = dp[i - 1] + dp[i - 5];
-        }
+        dp[0] = 0L;
+        dp[1] = 1L;
+        dp[2] = 1L;
+        dp[3] = 1L;
+        dp[4] = 2L;
+        dp[5] = 2L;
 
         while (t-- > 0) {
             int n = Integer.parseInt(br.readLine());
+            padoBan(n);
             System.out.println(dp[n]);
         }
 
+    }
+
+    private static Long padoBan(int index) {
+
+        if (dp[index] == null) {
+            dp[index] = padoBan(index -1) + padoBan(index -5);
+        }
+
+        return dp[index];
     }
 
 }
