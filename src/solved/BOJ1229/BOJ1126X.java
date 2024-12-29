d5ca3689307166c7ebf3c14ebdd392b222c33515
유종화 <sola201@naver.com>
2024-12-29 15:05:10 +0900

    다시 차근 차근

--- src/solved/BOJ1229/BOJ1126X.java
+++ src/solved/BOJ1229/BOJ1126X.java
@@ -5,6 +5,13 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+
+/*
+*
+*   행 과 열로 각각의 최대값을 비교하는 로직을 이해 못했음
+*
+* */
+
 public class BOJ1126X {
 
     public static void main(String[] args) throws IOException {
