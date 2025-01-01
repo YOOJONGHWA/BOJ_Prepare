427377120347c1a8a1990f7542dd2ae3a5936a45
유종화 <sola201@naver.com>
2025-01-01 18:08:15 +0900

    다시 차근 차근

--- src/solved/BOJ0101/BOJ1253X.java
+++ src/solved/BOJ0101/BOJ1253X.java
@@ -6,6 +6,25 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
+
+/*
+*
+*   이 문제의 핵심은 다른 두 수의 합으로 표현된다는 조건에 따라 자신을 포함하지 않는 두 수를 찾아야 한다는 점
+*   왜 자신을 제외해야 하는가?
+*   문제 정의:
+*   "좋은 수"란 다른 두 수의 합으로 나타낼 수 있는 수입니다.
+*
+*   자신을 포함하면 생기는 문제:
+*
+*   만약 자신을 포함한다면, 자신과 같은 값으로 합을 만들 수 있습니다.
+*   예를 들어, 배열에 [2, 2, 3]이 있을 때:
+*   2 + 2 = 4가 성립한다고 하면, 자기 자신과 합을 만드는 경우가 발생합니다.
+*
+*   하지만 문제의 본질은 배열의 다른 두 요소를 이용해서 합을 구성하는 것입니다.
+*
+*
+* */
+
 public class BOJ1253X {
 
     public static void main(String[] args) throws IOException {
@@ -22,35 +41,39 @@ public class BOJ1253X {
         Arrays.sort(arr);
 
         int count = 0;
-
         for (int i = 0; i < n; i++) {
+
+            int target = arr[i];
             int left = 0;
             int right = n - 1;
 
             while (left < right) {
-                // 자신을 제외한 두 수를 선택해야 하므로
-                if (left == i) {
+
+                if (i == left) {
                     left++;
                     continue;
                 }
-                if (right == i) {
+
+                if (i == right) {
                     right--;
                     continue;
                 }
 
                 int sum = arr[left] + arr[right];
-
-                if (sum == arr[i]) { // 좋은 수 발견
+                if (sum == target) {
                     count++;
                     break;
-                } else if (sum < arr[i]) { // 합이 작으면 왼쪽 포인터 이동
+                }
+                else if (sum < target) {
                     left++;
-                } else { // 합이 크면 오른쪽 포인터 이동
+                }
+                else {
                     right--;
                 }
-            }
-        }
 
+            }
+
+        }
         System.out.println(count);
     }
 }
