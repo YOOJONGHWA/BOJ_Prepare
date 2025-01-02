08e34580ac52889b4f801bfc183f4862b9dcf164
유종화 <sola201@naver.com>
2025-01-02 18:01:10 +0900

    다시 차근 차근

---
+++ src/solved/BOJ0102/BOJ1874.java
@@ -0,0 +1,45 @@
+package solved.BOJ0102;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+
+public class BOJ1874 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        StringBuilder sb = new StringBuilder();
+
+        int count = 1;
+        for (int i = 0; i < n; i++) {
+            int target = arr[i];
+
+            while (count <= target) {
+                stack.push(count++);
+                sb.append("+\n");
+            }
+
+            if (stack.peek() == target) {
+                stack.pop();
+                sb.append("-\n");
+            }
+            else {
+                System.out.println("NO");
+                return;
+            }
+
+        }
+        System.out.println(sb);
+    }
+
+}
