ae6fdbd13feee5aa795ee0b04b2db86d917b21bc
유종화 <sola201@naver.com>
2025-01-10 14:40:45 +0900

    다시 차근 차근

--- src/solved/BOJ0109/BOJ2023.java
+++ src/solved/BOJ0109/BOJ2023.java
@@ -1,16 +1,16 @@
 package solved.BOJ0109;
 
-import java.io.IOException;
 import java.util.Scanner;
 
 public class BOJ2023 {
 
     static int N;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt();
+
         DFS(2, 1);
         DFS(3, 1);
         DFS(5, 1);
@@ -22,9 +22,7 @@ public class BOJ2023 {
 
         if (jarisu == N) {
 
-            if (isPrime(number)) {
-                System.out.println(number);
-            }
+            if (isPrime(number)) System.out.println(number);
             return;
         }
 
@@ -33,20 +31,26 @@ public class BOJ2023 {
             if (i % 2 == 0) continue;
 
             if (isPrime(number * 10 + i)) {
+
                 DFS(number * 10 + i, jarisu + 1);
+
             }
+
         }
 
     }
 
-    private static boolean isPrime(int num) {
-        for (int i = 2; i <= num /2; i++) {
+    private static boolean isPrime(int number) {
 
-            if (num % i == 0) {
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+
+            if (number % i == 0) {
                 return false;
             }
+
         }
         return true;
+
     }
 
 }
