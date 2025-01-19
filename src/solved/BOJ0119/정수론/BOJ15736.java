ff078ca37fbe967d92881aaa7c760c9b06a15137
유종화 <sola201@naver.com>
2025-01-19 22:58:15 +0900

    다시 차근 차근

---
+++ src/solved/BOJ0119/정수론/BOJ15736.java
@@ -0,0 +1,15 @@
+package solved.BOJ0119.정수론;
+
+import java.util.Scanner;
+
+public class BOJ15736 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        long result = (long)Math.sqrt(n);
+        System.out.println(result);
+    }
+}
